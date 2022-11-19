59bf4fb9bbf46af38061b822784aeaf21ec875b4
AnzhelaSemkina <inforce888@gmail.com>
2022-11-19 19:52:32 +0300

    added bin/gendiff.js

---
+++ bin/gendiff.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath2> <filepath2>');
+
+program.parse();
