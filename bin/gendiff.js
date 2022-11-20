cf025c675d2112328eb60c07151321e2de7243df
AnzhelaSemkina <inforce888@gmail.com>
2022-11-20 22:55:56 +0300

    update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import gendiff from '../src/index.js';
 
 const program = new Command();
 
@@ -8,6 +9,9 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0', '-V, --version', 'output the version number')
   .option('-f, --format <type>', 'output format')
-  .argument('<filepath2> <filepath2>');
+  .arguments('<filepath1> <filepath2>')
+  .action((file1, file2) => {
+    console.log(gendiff(file1, file2));
+  });
 
 program.parse();
