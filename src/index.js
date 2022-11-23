68e9e2cf67017c051e157054384b457b288aa0d7
AnzhelaSemkina <inforce888@gmail.com>
2022-11-23 14:04:33 +0300

    fix index.js

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import _ from 'lodash';
 const getAbsoluteFilePath = (filePath) => path.resolve(filePath);
 const readFile = (filePath) => fs.readFileSync(getAbsoluteFilePath(filePath), 'utf8');
 const parseFile = (filePath) => JSON.parse(readFile(filePath));
-const getFormat = (filePath) => {
-  const name = filePath.split('/').at(-1);
-  const format = name.split('.').at(-1);
-  return format;
-};
+// const getFormat = (filePath) => {
+//   const name = filePath.split('/').at(-1);
+//   const format = name.split('.').at(-1);
+//   return format;
+// };
 
 const gendiff = (filePath1, filePath2) => {
   const data1 = parseFile(filePath1);
@@ -19,25 +19,23 @@ const gendiff = (filePath1, filePath2) => {
   const keys2 = Object.keys(data2);
   const keys = _.union(keys1, keys2);
 
-  const result = keys.map((key) => {
-    const res = [];
-    if (!Object.hasOwn(data1, key)) {
-      res.push(`+ ${key}`, `${data2[key]}`);
-    } else if (!Object.hasOwn(data2, key)) {
-      res.push(`- ${key}`, `${data1[key]}`);
-    } else if (data1[key] !== data2[key]) {
-      res.push(`- ${key}`, `${data1[key]}`);
-      res.push(`+ ${key}`, `${data2[key]}`);
-    } else {
-      res.push(`  ${key}`, `${data2[key]}`);
-    }
-    return res;
-  });
-  const res = {};
-  result.forEach(([key, value]) => {
-    res[key] = value;
-    return res;
-  });
-  return JSON.stringify(res, null, ' ');
+  const result = keys.sort()
+    .flatMap((key) => {
+      const res = [];
+      if (!Object.hasOwn(data1, key)) {
+        res.push(`+ ${key}: ${data2[key]}\n`);
+      } else if (!Object.hasOwn(data2, key)) {
+        res.push(`- ${key}: ${data1[key]}\n`);
+      } else if (data1[key] !== data2[key]) {
+        res.push(`- ${key}: ${data1[key]}\n`);
+        res.push(`+ ${key}: ${data2[key]}\n`);
+      } else {
+        res.push(`  ${key}: ${data2[key]}\n`);
+      }
+      return res;
+    });
+  const res = `{
+${result}}`;
+  return res.replace(/[",]/g, '');
 };
 export default gendiff;
