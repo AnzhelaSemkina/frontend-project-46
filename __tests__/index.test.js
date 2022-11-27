fdbd6147dd19f715cd508672738eca2f09b858ad
AnzhelaSemkina <inforce888@gmail.com>
2022-11-28 00:30:27 +0300

    update test.index.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,8 +9,14 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('gendiff', () => {
+test('json', () => {
   const expectedData = readFile('expectedFile.txt');
   const actualData = gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
+  expect(actualData).toEqual(expectedData);
+});
+
+test('yaml', () => {
+  const expectedData = readFile('expectedFile.txt');
+  const actualData = gendiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'));
   expect(actualData).toEqual(expectedData);
 });
