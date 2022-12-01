fe95f49dfbb184f63a8eee3b991de87d093c2a6f
AnzhelaSemkina <inforce888@gmail.com>
2022-12-01 22:42:53 +0300

    update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,10 +8,10 @@ const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0', '-V, --version', 'output the version number')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format(stylish, plain, json)', 'stylish')
   .arguments('<filepath1> <filepath2>')
-  .action((file1, file2) => {
-    console.log(gendiff(file1, file2));
+  .action((file1, file2, formatType) => {
+    console.log(gendiff(file1, file2, formatType));
   });
 
 program.parse();
