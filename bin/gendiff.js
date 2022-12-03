2054dda7f41db522ad356ee730877b059cdf30a1
AnzhelaSemkina <inforce888@gmail.com>
2022-12-03 21:48:17 +0300

    update gendiff.js for added options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,8 +10,8 @@ program
   .version('1.0.0', '-V, --version', 'output the version number')
   .option('-f, --format <type>', 'output format(stylish, plain, json)', 'stylish')
   .arguments('<filepath1> <filepath2>')
-  .action((file1, file2, formatType) => {
-    console.log(gendiff(file1, file2, formatType));
+  .action((file1, file2) => {
+    console.log(gendiff(file1, file2, program.opts().format));
   });
 
 program.parse();
