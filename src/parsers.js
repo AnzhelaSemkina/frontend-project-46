4f327cc97bee406872534112a6c5c5af607183e5
AnzhelaSemkina <inforce888@gmail.com>
2022-12-03 21:50:01 +0300

    fix parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -2,7 +2,7 @@ import * as fs from 'node:fs';
 import path from 'path';
 import yaml from 'js-yaml';
 
-const getAbsoluteFilePath = (filePath) => path.resolve(filePath);
+const getAbsoluteFilePath = (filePath) => path.resolve(process.cwd(), filePath);
 const readFile = (filePath) => fs.readFileSync(getAbsoluteFilePath(filePath), 'utf8');
 const parseFileJson = (filePath) => JSON.parse(readFile(filePath));
 const parseFileYaml = (filePath) => yaml.load(readFile(filePath));
