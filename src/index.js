5a8e639514e7b6f7ed6bfc7389d00f52aff7fab1
AnzhelaSemkina <inforce888@gmail.com>
2022-12-06 23:22:47 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import formatter from './formatter.js';
 const gendiff = (filePath1, filePath2, formatType = 'stylish') => {
   const data1 = parsers(filePath1);
   const data2 = parsers(filePath2);
-  return formatter(diff(data1, data2));
+  return formatter(diff(data1, data2), formatType);
 };
 
 export default gendiff;
