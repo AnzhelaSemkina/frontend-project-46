673fb8406bc840b3681abfd4e785cadfe7730af3
AnzhelaSemkina <inforce888@gmail.com>
2022-12-07 14:46:55 +0300

    fix /src/formatters/stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,4 +1,21 @@
+const stringify = (value, replacer = ' ', spacesCount = 1) => {
+  const iter = (currentValue, depth) => {
+    if (typeof currentValue !== 'object' || currentValue === null) {
+      return String(currentValue);
+    }
+    const indentSize = depth * spacesCount;
+    const currentIndent = replacer.repeat(indentSize);
+    const bracketIndent = replacer.repeat(indentSize - spacesCount);
+    const entries = Object.entries(currentValue);
+    const lines = entries.map(([key, val]) => `${currentIndent}${key}: ${iter(val, depth + 1)}`);
+    const result = ['{', ...lines, `${bracketIndent}}`].join('\n');
+    return result;
+  };
+  return iter(value, 1);
+};
+
 export default (diff) => {
-  const result = `  ${diff}`.replace(/[",]/g, '  ');
+  const result = stringify(diff);
+  // `  ${diff}`.replace(/[",]/g, '  ');
   return result;
 };
