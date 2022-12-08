f49f1d4fe7af9288460e074e1b459794fbfe8e4a
AnzhelaSemkina <inforce888@gmail.com>
2022-12-08 16:54:48 +0300

    fix src/buildTree.js

--- src/buildTree.js
+++ src/buildTree.js
@@ -8,20 +8,20 @@ const diff = (data1, data2) => {
   const result = keys.sort()
     .map((key) => {
       if (_.isObject(data1[key]) && _.isObject(data2[key])) {
-        return { key, type: 'nested', value: diff(data1[key], data2[key]) };
+        return [{ key, type: 'nested', value: diff(data1[key], data2[key]) }];
       }
       if (!Object.hasOwn(data1, key)) {
-        return { key, type: 'added', value: data2[key] };
+        return [{ key, type: 'added', value: data2[key] }];
       }
       if (!Object.hasOwn(data2, key)) {
-        return { key, type: 'deleted', value: data1[key] };
+        return [{ key, type: 'deleted', value: data1[key] }];
       }
       if (data1[key] !== data2[key]) {
-        return {
+        return [{
           key, type: 'changed', value1: data1[key], value2: data2[key],
-        };
+        }];
       }
-      return { key, type: 'unchanged', value: data2[key] };
+      return [{ key, type: 'unchanged', value: data2[key] }];
     });
 
   return result;
