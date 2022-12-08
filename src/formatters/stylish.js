ee3a6739281cfb3964eb5510033fae31c4c7f6a8
AnzhelaSemkina <inforce888@gmail.com>
2022-12-08 21:18:09 +0300

    fix src/formatters/stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,39 +1,48 @@
 const stringify = (value, replacer = ' ', spacesCount = 1) => {
   const iter = (currentValue, depth) => {
-    console.log(currentValue);
+    console.log(1, currentValue);
     if (typeof currentValue !== 'object' || currentValue === null) {
       return String(currentValue);
     }
+
     const indentSize = depth * spacesCount;
-    const currentIndent = replacer.repeat(indentSize);
+    const indent = replacer.repeat(indentSize);
     const bracketIndent = replacer.repeat(indentSize - spacesCount);
-    const fg = currentValue.flat();
-    console.log(fg);
-    const lines = fg.map((key) => {
-      console.log(key.type, key.key, key.value);
-      switch (key.type) {
-        case 'nested':
-          return `${currentIndent}  ${key.key}: ${iter(key.value, depth + 1)}`;
-        case 'deleted':
-          return `${currentIndent}- ${key.key}: ${iter(key.value, depth + 1)}`;
-        case 'added':
-          return `${currentIndent}+ ${key.key}: ${iter(key.value, depth + 1)}`;
-        case 'unchanged':
-          return `${currentIndent}  ${key.key}: ${iter(key.value, depth + 1)}`;
-        case 'changed':
-          return `${currentIndent}- ${key.key}: ${iter(key.value1, depth + 1)}\n${currentIndent}+ ${key.key}: ${iter(key.value2, depth + 1)}`;
-        default:
-          throw new Error(`Invalid data: ${key.type}, ${key.key}: ${JSON.stringify(key.value)}`);
-      }
-    });
-    console.log(lines);
-    const result = ['{', ...lines, `${bracketIndent}}`].join('\n');
-    return result;
+
+    const lines = currentValue
+      .flat()
+      .map((val) => {
+        console.log(2, val);
+        return `${indent}${val.key}: ${iter(val.value, depth + 1)}`;
+      });
+
+    return [
+      '{',
+      ...lines,
+      `${bracketIndent}}`,
+    ].join('\n');
   };
+  console.log(3, value);
   return iter(value, 1);
 };
 
-export default (diff) => {
-  const result = stringify(diff);
+export default (diff, indent, depth) => {
+  const result = diff.flatMap((value) => {
+    switch (value.type) {
+      case 'nested':
+        return `${indent}  ${value.key}: ${stringify(value.value, depth + 1)}`;
+      case 'deleted':
+        return `${indent}- ${value.key}: ${stringify(value.value, depth + 1)}`;
+      case 'added':
+        return `${indent}+ ${value.key}: ${stringify(value.value, depth + 1)}`;
+      case 'unchanged':
+        return `${indent}  ${value.key}: ${stringify(value.value, depth + 1)}`;
+      case 'changed':
+        return `${indent}- ${value.key}: ${stringify(value.value1, depth + 1)}\n${indent}+ ${value.key}: ${stringify(value.value2, depth + 1)}`;
+      default:
+        throw new Error(`Invalid data: ${value.type}, ${value.key}: ${JSON.stringify(value.value)}`);
+    }
+  });
+  console.log(result);
   return result;
 };
