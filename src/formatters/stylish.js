194562baaeaf9b36066928c757e8f564f958f09c
AnzhelaSemkina <inforce888@gmail.com>
2022-12-09 22:59:37 +0300

    fix src/formatters/stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,48 +1,57 @@
 const stringify = (value, replacer = ' ', spacesCount = 1) => {
   const iter = (currentValue, depth) => {
-    console.log(1, currentValue);
-    if (typeof currentValue !== 'object' || currentValue === null) {
-      return String(currentValue);
-    }
-
+    // console.log(1, currentValue);
     const indentSize = depth * spacesCount;
     const indent = replacer.repeat(indentSize);
     const bracketIndent = replacer.repeat(indentSize - spacesCount);
 
+    if (typeof currentValue !== 'object' || currentValue === null) {
+      console.log(2, 'return', currentValue);
+      return `${currentValue}`;
+    }
     const lines = currentValue
-      .flat()
       .map((val) => {
-        console.log(2, val);
+        console.log(3, val, `${indent}${val.key}: ${iter(val.value, depth + 1)}`);
         return `${indent}${val.key}: ${iter(val.value, depth + 1)}`;
       });
-
+    console.log(4, lines);
+    console.log(5, currentValue);
     return [
       '{',
       ...lines,
       `${bracketIndent}}`,
     ].join('\n');
   };
-  console.log(3, value);
+
   return iter(value, 1);
 };
 
-export default (diff, indent, depth) => {
-  const result = diff.flatMap((value) => {
-    switch (value.type) {
+export default (diff, depth = 2, replacer = ' ', spacesCount = 1) => {
+  const indentSize = depth * spacesCount;
+  const indent = replacer.repeat(indentSize);
+  const bracketIndent = replacer.repeat(indentSize - spacesCount);
+
+  const result = diff.flatMap((val) => {
+    console.log(6, val);
+    switch (val.type) {
       case 'nested':
-        return `${indent}  ${value.key}: ${stringify(value.value, depth + 1)}`;
+        return `${indent}  ${val.key}: ${stringify(val.value, ' ', depth + 1)}`;
       case 'deleted':
-        return `${indent}- ${value.key}: ${stringify(value.value, depth + 1)}`;
+        return `${indent}- ${val.key}: ${stringify(val.value, ' ', depth + 1)}`;
       case 'added':
-        return `${indent}+ ${value.key}: ${stringify(value.value, depth + 1)}`;
+        return `${indent}+ ${val.key}: ${stringify(val.value, ' ', depth + 1)}`;
       case 'unchanged':
-        return `${indent}  ${value.key}: ${stringify(value.value, depth + 1)}`;
+        return `${indent}  ${val.key}: ${stringify(val.value, ' ', depth + 1)}`;
       case 'changed':
-        return `${indent}- ${value.key}: ${stringify(value.value1, depth + 1)}\n${indent}+ ${value.key}: ${stringify(value.value2, depth + 1)}`;
+        return `${indent}- ${val.key}: ${stringify(val.value1, ' ', depth + 1)}\n${indent}+ ${val.key}: ${stringify(val.value2, ' ', depth + 1)}`;
       default:
-        throw new Error(`Invalid data: ${value.type}, ${value.key}: ${JSON.stringify(value.value)}`);
+        throw new Error(`Invalid data: ${val.type}, ${val.key}: ${JSON.stringify(val.value)}`);
     }
   });
-  console.log(result);
-  return result;
+  console.log(7, result);
+  return [
+    '{',
+    ...result,
+    `${bracketIndent}}`,
+  ].join('\n');
 };
