aeedb1c74aa4a09c0281744b975cfecc33f2f967
AnzhelaSemkina <inforce888@gmail.com>
2022-12-11 00:58:09 +0300

    fix src/buildTree.js

--- src/buildTree.js
+++ src/buildTree.js
@@ -3,8 +3,7 @@ import _ from 'lodash';
 const diff = (data1, data2) => {
   const keys1 = Object.keys(data1);
   const keys2 = Object.keys(data2);
-  const result = _.union(keys1, keys2)
-    .sort()
+  const result = _.sortBy(_.union(keys1, keys2))
     .map((key) => {
       if (_.isObject(data1[key]) && _.isObject(data2[key])) {
         return [key, { type: 'nested', value: diff(data1[key], data2[key]) }];
