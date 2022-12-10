743dadd3426700bfae9059c6eba4d812e7dff671
AnzhelaSemkina <inforce888@gmail.com>
2022-12-11 00:39:51 +0300

    fix src/formatters/stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -7,7 +7,7 @@ const objectToString = (value, currentDepth, replacer = ' ', spacesCount = 1) =>
 
 const stringify = (value) => {
   const iter = (currentValue, depth) => {
-    console.log('currenValue:', currentValue);
+    console.log('currentValue:', currentValue);
 
     if (typeof currentValue !== 'object' || currentValue === null) {
       console.log('return', currentValue);
@@ -15,13 +15,8 @@ const stringify = (value) => {
     }
 
     const lines = [currentValue]
-      .map((val) => objectToString(`${val.key}: ${iter(val.value, depth + 1)}`));
-    console.log(4, lines);
-    console.log(5, currentValue);
-    return [
-      '{',
-      ...lines,
-    ].join('\n');
+      .map((key) => objectToString(`${key[0]}: ${iter(key[1])}`, depth, '  ', 1));
+    return lines;
   };
 
   return iter(value, 1);
@@ -30,23 +25,27 @@ const stringify = (value) => {
 const stylish = (diff, depth) => {
   // const bracketIndent = replacer.repeat(indentSize - spacesCount);
 
-  const result = diff.flatMap((val) => {
-    console.log('resultValue', val);
+  const result = diff.flatMap(([key, val]) => {
+    console.log('resultValue', [key, val]);
     switch (val.type) {
       case 'nested':
-        return `  ${val.key}: ${stylish(val.value, ' ', depth + 1)}`;
+        return `  ${key}: ${stylish(val.value, ' ', depth + 1)}`;
       case 'deleted':
-        return `- ${val.key}: ${stringify(val.value, ' ', depth + 1)}`;
+        return `- ${key}: ${stringify(val.value, ' ', depth + 1)}`;
       case 'added':
-        return `+ ${val.key}: ${stringify(val.value, ' ', depth + 1)}`;
+        return `+ ${key}: ${stringify(val.value, ' ', depth + 1)}`;
       case 'changed':
-        return `- ${val.key}: ${stringify(val.value1, ' ', depth + 1)}\n+ ${val.key}: ${stringify(val.value2, ' ', depth + 1)}`;
+        return `- ${key}: ${stringify(val.value1, ' ', depth + 1)}\n+ ${key}: ${stringify(val.value2, ' ', depth + 1)}`;
       default:
-        return `  ${val.key}: ${stringify(val.value, ' ', depth + 1)}`;
+        return `  ${key}: ${stringify(val.value, ' ', depth + 1)}`;
     }
   });
   console.log(7, result);
-  return result.join('\n');
+  return [
+    '{',
+    ...result,
+    '}',
+  ].join('\n');
 };
 
 export default stylish;
