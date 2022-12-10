fbdfb69d1f4134e5a12a7332a00cb48d340341f2
AnzhelaSemkina <inforce888@gmail.com>
2022-12-11 00:40:17 +0300

    fix src/buildTree.js

--- src/buildTree.js
+++ src/buildTree.js
@@ -4,29 +4,23 @@ const diff = (data1, data2) => {
   const keys = _.union(Object.keys(data1), Object.keys(data2));
 
   const result = keys.sort()
-    .reduce((acc, key) => {
+    .map((key) => {
       if (_.isObject(data1[key]) && _.isObject(data2[key])) {
-        acc.push({ key, type: 'nested', value: diff(data1[key], data2[key]) });
-        return acc;
+        return [key, { type: 'nested', value: diff(data1[key], data2[key]) }];
       }
       if (!Object.hasOwn(data1, key)) {
-        acc.push({ key, type: 'added', value: data2[key] });
-        return acc;
+        return [key, { type: 'added', value: data2[key] }];
       }
       if (!Object.hasOwn(data2, key)) {
-        acc.push({ key, type: 'deleted', value: data1[key] });
-        return acc;
+        return [key, { type: 'deleted', value: data1[key] }];
       }
       if (data1[key] !== data2[key]) {
-        acc.push({
-          key, type: 'changed', value1: data1[key], value2: data2[key],
-        });
-        return acc;
+        return [
+          key, { type: 'changed', value1: data1[key], value2: data2[key] }];
       }
-      acc.push({ key, type: 'unchanged', value: data2[key] });
-      return acc;
-    }, []);
-  console.log(result);
+      return [key, { type: 'unchanged', value: data2[key] }];
+    });
+  console.log('diff', result);
   return result;
 };
 
