5c7a4119f66c903237b7fb9b2a9d0ccf94ec3a37
AnzhelaSemkina <inforce888@gmail.com>
2022-12-13 16:03:46 +0300

    fix src/formatters/stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,53 +1,48 @@
+const replacer = ' ';
+const spacesCount = 2;
+
 const makeObjectToString = (currentValue, level) => {
-  const indent = '  '.repeat(level);
-  return `${indent} ${currentValue}`;
-};
-const stringify = (value, replacer = '  ', spacesCount = 1) => {
-  const iter = (currentValue, depth) => {
-    if (typeof currentValue !== 'object' || currentValue === null) {
-      return `${currentValue}`;
-    }
-
-    const indentSize = depth * spacesCount;
-    const indent = replacer.repeat(indentSize);
-    const bracketIndent = replacer.repeat(indentSize - spacesCount);
-    console.log(currentValue);
-    const lines = Object.entries(currentValue)
-      .map(([key, val]) => makeObjectToString(`${key}: ${iter(val, depth + 1)}`, depth + 1));
-    console.log('lines', lines);
-
+  const indentSize = level * spacesCount;
+  const indent = replacer.repeat(indentSize);
+  const bracketIndent = replacer.repeat(indentSize - spacesCount);
+  if (typeof currentValue === 'object') {
     return [
       '{',
-      ...lines,
+      ...currentValue,
       `${bracketIndent}}`,
     ].join('\n');
-  };
+  }
+  return `${indent}${currentValue}`;
+};
 
-  return iter(value, 1);
+const stringify = (value, depth) => {
+  if (typeof value !== 'object' || value === null) {
+    return `${value}`;
+  }
+
+  const lines = Object.entries(value)
+    .map(([key, val]) => makeObjectToString(`${key}: ${stringify(val, depth + 1)}`, depth + 2));
+  console.log('lines', lines);
+  return makeObjectToString(lines, depth + 1);
 };
 
 const stylish = (diff, depth) => {
-  const currentIndent = '  '.repeat(depth);
   const result = diff.map(([key, val]) => {
     switch (val.type) {
       case 'nested':
-        return makeObjectToString(`  ${key}: ${stylish(val.value, depth + 1)}`, depth);
+        return makeObjectToString(`${key}: ${stylish(val.value, depth + 2)}`, depth + 1);
       case 'deleted':
-        return `${currentIndent}- ${key}: ${stringify(val.value, '  ', depth + 1)}`;
+        return makeObjectToString(`- ${key}: ${stringify(val.value, depth + 1)}`, depth);
       case 'added':
-        return `${currentIndent}+ ${key}: ${stringify(val.value, '  ', depth + 1)}`;
+        return makeObjectToString(`+ ${key}: ${stringify(val.value, depth + 1)}`, depth);
       case 'changed':
-        return `${currentIndent}- ${key}: ${stringify(val.value1, '  ', depth + 1)}\n${currentIndent}+ ${key}: ${stringify(val.value2, '  ', depth + 1)}`;
+        return `${makeObjectToString(`- ${key}: ${stringify(val.value1, depth + 1)}`, depth)}\n${makeObjectToString(`+ ${key}: ${stringify(val.value2, depth + 1)}`, depth)}`;
       default:
-        return `${currentIndent}  ${key}: ${stringify(val.value, '  ', depth + 1)}`;
+        return makeObjectToString(`  ${key}: ${stringify(val.value, depth + 1)}`, depth);
     }
   });
   console.log('resultStylish', result);
-  return [
-    '{',
-    ...result,
-    '}',
-  ].join('\n');
+  return makeObjectToString(result, depth);
 };
 
 export default stylish;
