b02cb18a92da8bbb240948846f0f586ecdbdf67f
AnzhelaSemkina <inforce888@gmail.com>
2022-12-17 20:28:01 +0300

    fix src/index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
+import * as fs from 'node:fs';
+import path from 'path';
 import parsers from './parsers.js';
 import diff from './buildTree.js';
 import formatter from './formatters/index.js';
 
+const getAbsoluteFilePath = (filePath) => path.resolve(process.cwd(), filePath);
+const readFile = (filePath) => fs.readFileSync(filePath, 'utf8');
+const getFormat = (filePath) => path.extname(filePath);
+
 const gendiff = (filePath1, filePath2, formatType = 'stylish') => {
-  const data1 = parsers(filePath1);
-  const data2 = parsers(filePath2);
-  return formatter(diff(data1, data2), formatType);
+  const data1 = readFile(getAbsoluteFilePath(filePath1));
+  const data2 = readFile(getAbsoluteFilePath(filePath2));
+
+  const dataParse1 = parsers(data1, getFormat(filePath1));
+  const dataParse2 = parsers(data2, getFormat(filePath2));
+
+  return formatter(diff(dataParse1, dataParse2), formatType);
 };
 
 export default gendiff;
